65303408bafb5d85d4e23c8898a3afe5137aeb28
AndraCarla <113671176+AndraCarla@users.noreply.github.com>
2023-12-15 20:15:52 +0100

    fix: models for booking

--- booking-master/src/main/java/fr/pantheonsorbonne/ufr27/miage/model/Reservation.java
+++ booking-master/src/main/java/fr/pantheonsorbonne/ufr27/miage/model/Reservation.java
@@ -1,8 +1,8 @@
 package fr.pantheonsorbonne.ufr27.miage.model;
 
 import jakarta.persistence.*;
-
 import java.util.Date;
+import java.util.Set;
 
 @Table(name = "Reservation")
 @Entity
@@ -11,38 +11,52 @@ public class Reservation {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "idReservation", nullable = false)
     private Integer id;
-
-    @Column(name = "number_Guests", nullable = false)
-    private Integer guestsNumber;
-    @Column(name = "booked_date", nullable = false)
-    private Date bookingDate;
-    @Column(name = "start_date", nullable = false)
-    private Date startDate;
-
-    @Column(name = "end_date", nullable = false)
-    private Date endDate;
+    @ManyToOne
+    @JoinColumn(name = "hotelId")
+    private Hotel hotel;
+    @ManyToOne
+    @JoinColumn(name = "userId")
+    private User user;
+    @ManyToMany
+    @JoinTable(name="ReservationOptions")
+    private Set<HotelOption> options;
+    public Set<HotelOption> getOptions() {
+        return options;
+    }
+    public void setOptions(Set<HotelOption> options) {
+        this.options= options;
+    }
     @Column(name = "status", nullable = false)
     private String status ;
-
-    @Column(name = "hotel_id", nullable = false)
-    private Integer hotelId ;
-
-    @Column(name = "reservetionRoom_id", nullable = false)
-    private Integer reservetionRoomId ;
-
-    public Integer getGuestsNumber() {
-        return guestsNumber;
-    }
-
-    public void setGuestsNumber(Integer number) {
-        this.guestsNumber = number;
-    }
-
+    @Column(name = "startDate", nullable = false)
+    private Date startDate;
+    @Column(name = "endDate", nullable = false)
+    private Date endDate;
+    @Column(name = "bedsNumber", nullable = false)
+    private Integer bedsNumber;
+    @Column(name = "roomsNumber", nullable = false)
+    private Integer roomsNumber;
+    @Column(name = "bookingDate")
+    private Date bookingDate;
     public Integer getId() {
         return id;
     }
-
     public void setId(Integer id) {
         this.id = id;
     }
+    public String getStatus(){return status;}
+    public void setStatus(String status) {this.status = status;}
+    public Date getStartDate(){return startDate;}
+    public void setStartDate(Date startDate) {this.startDate = startDate;}
+    public Date getEndDate(){return endDate;}
+    public void setEndDate(Date endDate) {this.endDate = endDate;}
+    public Integer getBedsNumber() {return bedsNumber;}
+    public void setBedsNumber(Integer bedsNumber){this.bedsNumber = bedsNumber;}
+    public Integer getRoomsNumber() {return roomsNumber;}
+    public void setRoomsNumber(Integer roomsNumber){this.roomsNumber = roomsNumber;}
+    public Date getBookingDate() {return bookingDate;}
+    public void setBookingDate(Date bookingDate){this.bookingDate = bookingDate;}
+    public User getUser() {return user;}
+    public void setUser(User user){this.user = user;}
+
 }
--- booking-master/src/main/resources/import.sql
+++ booking-master/src/main/resources/import.sql
@@ -1,1 +1,15 @@
-REPLACE INTO `HotelLocation` (`idLocation`, `locationName`, `latitude`, `longitude`) VALUES (1,'Bordeaux Akea Arena','0','0'), (2,'Paris','1','1');
+REPLACE INTO `HotelLocation` ( `locationName`, `latitude`, `longitude`) VALUES ('California','0','0'), ('Paris','1','1');
+REPLACE INTO `Hotel` (`locationId`,`hotelName`) VALUES (2,'Hotel Paradiso');
+REPLACE INTO `Hotel` (`locationId`,`hotelName`) VALUES (1,'Hotel California');
+REPLACE INTO `User` (`firstName`,`lastName`,`email`) VALUES ('Jean','Dupont','jeandupont@gmail.com');
+REPLACE INTO `HotelOption` (`optionName`) VALUES ('Spa');
+REPLACE INTO `HotelOption` (`optionName`) VALUES ('Pool');
+REPLACE INTO `HotelOption` (`optionName`) VALUES ('Parking');
+REPLACE INTO `HotelOptions` (`hotels_hotelId`,`options_optionId`) VALUES (1,1);
+REPLACE INTO `HotelOptions` (`hotels_hotelId`,`options_optionId`) VALUES (1,2);
+REPLACE INTO `HotelOptions` (`hotels_hotelId`,`options_optionId`) VALUES (2,2);
+REPLACE INTO `Availability` (`hotelId`,`numberFreeRooms`,`date`,`bedsNumber`) VALUES (1,5,'2023-12-31',2);
+REPLACE INTO `Availability` (`hotelId`,`numberFreeRooms`,`date`,`bedsNumber`) VALUES (1,3,'2023-12-31',3);
+REPLACE INTO `Availability` (`hotelId`,`numberFreeRooms`,`date`,`bedsNumber`) VALUES (1,7,'2024-01-01',2);
+REPLACE INTO `Availability` (`hotelId`,`numberFreeRooms`,`date`,`bedsNumber`) VALUES (2,10,'2023-12-31',2);
+REPLACE INTO `Availability` (`hotelId`,`numberFreeRooms`,`date`,`bedsNumber`) VALUES (2,2,'2023-12-31',3);
